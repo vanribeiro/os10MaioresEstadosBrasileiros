2a510c8d580e043c0262cf063fcd8366b78cf53a
Vanessa Ribeiro <Van Ribeiro>
2018-09-11 19:28:12 -0300

    Criei a exibição dos estados.

--- src/main/java/challenge/Main.java
+++ src/main/java/challenge/Main.java
@@ -28,11 +28,16 @@ public class Main {
 	}
 	
 	public static void StateResults() {
-		for(int i = 0; i < 10; i++) {
-			System.out.println(i+1 + " " +
-					state.get(i).getName() +
-					" " + state.get(i).getUf());
-		}
+		System.out.printf("-------------------------------------------------\n");
+		System.out.printf(" Os 10 maiores estados brasileiros - (1.000 Km\u00B2)\n");
+		System.out.printf("-------------------------------------------------\n");
+		System.out.printf("| Nº | Nome do Estado            | Área         |\n");
+		System.out.printf("-------------------------------------------------\n");
+		for(int i = 0; i < 10; i++){
+			System.out.printf("| %02d | %-18s\t | %s\t|\n", 
+					i+1, state.get(i).getName(), 
+					state.get(i).getUf().toString().replace(".", ","));
+		}System.out.printf("-------------------------------------------------\n");
 	}
 	
 	public static void main(String[] args) {
